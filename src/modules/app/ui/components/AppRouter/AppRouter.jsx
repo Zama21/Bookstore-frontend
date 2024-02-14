a4bfc189d439ed2be6b4c792116619aef692625c
pd125 <p@x128.ru>
2024-02-14 14:21:23 +0300

    basic routes

--- src/modules/app/ui/components/AppRouter/AppRouter.jsx
+++ src/modules/app/ui/components/AppRouter/AppRouter.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
-
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import { HomePage } from '../../../../home/ui/pages/HomePage/HomePage.jsx';
+import { BookPage } from '../../../../books/ui/pages/BookPage/BookPage.jsx';
+import { BookReadPage } from '../../../../books/ui/pages/BookReadPage/BookReadPage.jsx';
+import { RegPage } from '../../../../auth/ui/pages/RegPage/RegPage.jsx';
+import { AuthPage } from '../../../../auth/ui/pages/AuthPage/AuthPage.jsx';
 export const AppRouter = () => {
-    return <div></div>;
+    return createBrowserRouter(
+        createRoutesFromElements(
+            <Route path='/'>
+                <Route path='home' element={<HomePage />} />
+                <Route path='auth'>
+                    <Route path='reg' element={<RegPage />} />
+                    <Route path='login' element={<AuthPage />} />
+                </Route>
+                <Route path='book/:bookId' element={<BookPage />} />
+                <Route path='book/:bookId/read' element={<BookReadPage />} />
+            </Route>
+        )
+    );
 };
