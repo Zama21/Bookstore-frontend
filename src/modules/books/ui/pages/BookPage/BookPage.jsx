12c4c233a2e29a8e37047faa44587fffe4d8c052
Zama21 <zama.21@uni-dubna.ru>
2024-02-16 18:01:06 +0300

    инициализация ветки

--- src/modules/books/ui/pages/BookPage/BookPage.jsx
+++ src/modules/books/ui/pages/BookPage/BookPage.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import stl from './BookPage.module.css'
 
 export const BookPage = () => {
-    const { bookId } = useParams();
+	const { bookId } = useParams()
 
-    return (
-        <div>
-            <h1>Book with ID = {bookId}!</h1>
-        </div>
-    );
-};
+	return (
+		<div className='stl.page'>
+			<h1>Book with ID = {bookId}!</h1>
+		</div>
+	)
+}
