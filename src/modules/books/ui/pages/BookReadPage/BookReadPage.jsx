ee81b01db279dca3afc7f6c1a1d0416f295dc20b
Zama21 <zama.21@uni-dubna.ru>
2024-02-18 12:25:37 +0300

    начало работы над макетом читалки

--- src/modules/books/ui/pages/BookReadPage/BookReadPage.jsx
+++ src/modules/books/ui/pages/BookReadPage/BookReadPage.jsx
@@ -1,12 +1,62 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import stl from './BookReadPage.module.css'
 
 export const BookReadPage = () => {
-    const { bookId } = useParams();
+	const { bookId } = useParams()
 
-    return (
-        <div>
-            <h1>Read book with ID = {bookId}! </h1>
-        </div>
-    );
-};
+	return (
+		<>
+			<main>
+				<div className={stl.wrapper}>
+					<div>хлебные крошки</div>
+					<h1 className={stl.bookReadH1}>
+						<a href='http://localhost:5173/book/13'>
+							Тёмный Травник. Верховья Стикса
+						</a>
+					</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+						labore iusto, nesciunt recusandae fugiat quo ipsum cumque et
+						laudantium provident pariatur fugit quibusdam nemo debitis assumenda
+						aperiam minus voluptatibus quam. Rerum sequi voluptatibus
+						perferendis inventore omnis corporis consequuntur cumque adipisci
+						quia exercitationem iste commodi corrupti voluptatum, debitis
+						mollitia aperiam accusamus quidem. Quo facere perferendis natus unde
+						hic, recusandae velit minus nobis adipisci numquam quae cumque
+						consequuntur expedita, dolores consequatur iusto tempore commodi,
+						repellendus voluptatibus molestiae beatae? Ipsa, ipsum. Amet animi
+						beatae illo, iure ipsum omnis sint similique ducimus officiis quod
+						obcaecati eligendi dicta sapiente perspiciatis. Sed maiores libero
+						deleniti ullam laborum cupiditate, aliquam laudantium et commodi
+						minus dolorem temporibus earum voluptates, alias repudiandae
+						asperiores officiis itaque? Non aspernatur maiores dolore voluptates
+						assumenda a quibusdam neque. Ea, libero magni? Magnam, nisi
+						quibusdam similique voluptatum rerum exercitationem rem sit
+						dignissimos fugit aspernatur omnis velit nam voluptates qui a.
+						Aperiam modi animi eaque repellendus minus numquam excepturi harum
+						facilis nam illum aspernatur quos dolores eligendi quibusdam
+						consequuntur quia delectus optio culpa exercitationem, vel error
+						tempore dicta id? Quasi laboriosam autem repudiandae, ipsa earum
+						eveniet. Adipisci distinctio illum a quibusdam earum aperiam,
+						suscipit, ipsam porro sint ipsum quisquam repellat, assumenda itaque
+						nihil! Rerum nam, aliquid excepturi distinctio omnis, sit debitis
+						sunt praesentium obcaecati ullam quibusdam consequatur! Quasi
+						reprehenderit eum architecto dolorem sapiente recusandae quibusdam
+						neque reiciendis dolore ut at debitis aperiam soluta esse dolorum
+						expedita, iusto cumque. Perferendis dolorum sequi, repellendus,
+						ipsum fugiat ipsam itaque quam esse inventore animi sit similique,
+						cum hic quod possimus libero quo incidunt consequuntur? Doloribus
+						vel nesciunt nihil, eos dicta recusandae quibusdam, quae rem
+						deleniti odio accusantium voluptas ipsum praesentium nam maxime
+						doloremque libero hic tempore nobis ut enim voluptatum. Officiis
+						corporis odio voluptatem dignissimos facilis, fugiat reprehenderit,
+						excepturi provident quam rem voluptatibus incidunt deserunt nobis et
+						laborum vitae?
+					</p>
+				</div>
+			</main>
+			<footer></footer>
+		</>
+	)
+}
---
+++ src/modules/books/ui/pages/BookReadPage/BookReadPage.module.css.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+  readonly "bookReadH1": string;
+  readonly "wrapper": string;
+};
+export = styles;
+
