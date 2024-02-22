6b0392665fe3c9bfa49b8374b053d988ef17fdf3
Zama21 <zama.21@uni-dubna.ru>
2024-02-22 23:59:16 +0300

    все связанное с heder.

---
+++ src/modules/app/ui/components/GlobalHeader/svg/GlobalHeaderSvgSelector.module.css.d.ts
@@ -0,0 +1,20 @@
+declare const styles: {
+  readonly "bookLogo1": string;
+  readonly "bookLogo10": string;
+  readonly "bookLogo11": string;
+  readonly "bookLogo12": string;
+  readonly "bookLogo13": string;
+  readonly "bookLogo14": string;
+  readonly "bookLogo15": string;
+  readonly "bookLogo2": string;
+  readonly "bookLogo3": string;
+  readonly "bookLogo4": string;
+  readonly "bookLogo5": string;
+  readonly "bookLogo6": string;
+  readonly "bookLogo7": string;
+  readonly "bookLogo8": string;
+  readonly "bookLogo9": string;
+  readonly "logo": string;
+};
+export = styles;
+
