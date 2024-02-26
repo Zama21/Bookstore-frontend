9ca07e5986201644255529d1397d1b16630c8e54
pd125 <p@x128.ru>
2024-02-26 11:02:46 +0300

    animation mini change

--- src/shared/ui/components/Modal/Modal.jsx
+++ src/shared/ui/components/Modal/Modal.jsx
@@ -1,48 +1,40 @@
-import React, { useEffect } from 'react'
-import styles from './Modal.module.css'
-import ReactDOM from 'react-dom'
+import React, { useEffect } from 'react';
+import styles from './Modal.module.css';
+import ReactDOM from 'react-dom';
 
-const Backdrop = props => {
-	useEffect(() => {
-		document.body.style.overflow = 'hidden'
-		return () => (document.body.style.overflow = 'unset')
-	}, [])
-	let stl = `${styles.backdrop} ${props.closingAnimation ? styles.close : ''}`
-	return <div className={stl} onClick={props.onHideCart}></div>
-}
+const Backdrop = (props) => {
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+        return () => (document.body.style.overflow = 'unset');
+    }, []);
+    let stl = `${styles.backdrop} ${props.closingAnimation ? styles.close : ''}`;
+    return <div className={stl} onClick={props.onHideCart}></div>;
+};
 
-const ModalWindow = props => {
-	return (
-		<div
-			className={`${styles.modal} ${
-				props.closingAnimation ? styles.close : ''
-			}`}
-		>
-			<div className={`${styles.content} `}>{props.children}</div>
-		</div>
-	)
-}
+const ModalWindow = (props) => {
+    return (
+        <div className={`${styles.modal} ${props.closingAnimation ? styles.close : ''}`}>
+            <div className={`${styles.content} `}>{props.children}</div>
+        </div>
+    );
+};
 
-const portalElement = document.getElementById('overlays')
+const portalElement = document.getElementById('overlays');
+// const portalElement = document.body;
 
-const Modal = props => {
-	return (
-		<React.Fragment>
-			{ReactDOM.createPortal(
-				<Backdrop
-					onHideCart={props.onHideCart}
-					closingAnimation={props.closingAnimation}
-				/>,
-				portalElement
-			)}
-			{ReactDOM.createPortal(
-				<ModalWindow closingAnimation={props.closingAnimation}>
-					{props.children}
-				</ModalWindow>,
-				portalElement
-			)}
-		</React.Fragment>
-	)
-}
+const Modal = (props) => {
+    return (
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onHideCart={props.onHideCart} closingAnimation={props.closingAnimation} />,
+                portalElement
+            )}
+            {ReactDOM.createPortal(
+                <ModalWindow closingAnimation={props.closingAnimation}>{props.children}</ModalWindow>,
+                portalElement
+            )}
+        </React.Fragment>
+    );
+};
 
-export default Modal
+export default Modal;
